880703f23acce0a0cab0a538c75734a7cc78b91b
Alexandrea <54953677+s465640@users.noreply.github.com>
2020-01-02 18:30:31 -0500

    Added CPT

---
+++ SpaceInvadersAlex/SpaceInvadersAlex/SpaceInvadersForm.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace SpaceInvadersAlex
+{
+    partial class SpaceInvadersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.invader = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invader)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 414);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(99, 24);
+            this.lblScore.TabIndex = 14;
+            this.lblScore.Text = "Score: 00";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox7.Location = new System.Drawing.Point(1, 12);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "Invaders";
+            // 
+            // invader
+            // 
+            this.invader.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.invader.Location = new System.Drawing.Point(-268, -240);
+            this.invader.Name = "invader";
+            this.invader.Size = new System.Drawing.Size(72, 59);
+            this.invader.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.invader.TabIndex = 1;
+            this.invader.TabStop = false;
+            this.invader.Tag = "Invader";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.player.BackgroundImage = global::SpaceInvadersAlex.Properties.Resources.space_1;
+            this.player.Image = global::SpaceInvadersAlex.Properties.Resources.cann;
+            this.player.Location = new System.Drawing.Point(271, 411);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 50);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Tag = "Player";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(449, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "Invaders";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox2.Location = new System.Drawing.Point(393, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "Invaders";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox3.Location = new System.Drawing.Point(337, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "Invaders";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox4.Location = new System.Drawing.Point(281, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 18;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "Invaders";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox5.Location = new System.Drawing.Point(225, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 19;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "Invaders";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox6.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 20;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "Invaders";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox8.Location = new System.Drawing.Point(113, 12);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 21;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "Invaders";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox9.Location = new System.Drawing.Point(57, 12);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 22;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "Invaders";
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox11.Location = new System.Drawing.Point(622, 12);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 24;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Tag = "Invaders";
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox12.Location = new System.Drawing.Point(561, 12);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 25;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Tag = "Invaders";
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Image = global::SpaceInvadersAlex.Properties.Resources.images;
+            this.pictureBox13.Location = new System.Drawing.Point(505, 12);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox13.TabIndex = 26;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Tag = "Invaders";
+            // 
+            // SpaceInvadersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImage = global::SpaceInvadersAlex.Properties.Resources.space_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.invader);
+            this.Controls.Add(this.player);
+            this.Name = "SpaceInvadersForm";
+            this.Text = "Space Invaders by Alex";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invader)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox invader;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+    }
+}
+
---
+++ SpaceInvadersAlex/SpaceInvadersAlex/SpaceInvadersForm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceInvadersAlex
+{
+    public partial class SpaceInvadersForm : Form
+    {
+        bool goleft;
+        bool goright;
+        int speed = 5;
+        int score = 0;
+        bool isPressed;
+        int totalEnemies = 12;
+        int playerSpeed = 6;
+        public SpaceInvadersForm()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+            if (e.KeyCode == Keys.Space && !isPressed)
+            {
+                isPressed = true;
+                createBullet();
+                //do work
+            }
+
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+            if (isPressed)
+            {
+                isPressed = false;
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //player moving left and right
+            if (goleft)
+            {
+                player.Left -= playerSpeed;
+            }
+            else if (goright)
+            {
+                player.Left += playerSpeed;
+            }
+            //end of player moving left and right
+
+
+            //invaders moving on the form
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "invader")
+                {
+
+                    if (((PictureBox)x).Bounds.IntersectsWith(player.Bounds))
+                    {
+                        gameOver();
+                    }
+
+                   ((PictureBox)x).Left += speed;
+
+
+                    if (((PictureBox)x).Left > 720)
+                    {
+                        ((PictureBox)x).Top += ((PictureBox)x).Height + 10;
+                        ((PictureBox)x).Left = -50;
+
+                    }
+                }
+            }
+            //end of enemies moving on the form
+        
+            //animating the bullets and removing them when they have left the scene
+            foreach (Control y in this.Controls)
+            {
+                if (y is PictureBox && y.Tag == "bullet")
+                {
+                    y.Top -= 20;
+                    if (((PictureBox)y).Top < this.Height - 490)
+                    {
+                        this.Controls.Remove(y);
+                    }
+                }
+            }
+            //bullet and enemy collision start
+            foreach (Control i in this.Controls)
+            {
+                foreach (Control j in this.Controls)
+                {
+                    if (i is PictureBox && i.Tag == "invader")
+                    {
+                        if (j is PictureBox && j.Tag == "bullet")
+                        {
+                            if (i.Bounds.IntersectsWith(j.Bounds))
+                            {
+                                score++;
+                                this.Controls.Remove(i);
+                                this.Controls.Remove(j);
+                            }
+                        }
+                    }
+                }
+            }
+            //keeping and showing score
+            lblScore.Text = "Score : " + score;
+            if (score > totalEnemies - 1)
+            {
+                gameOver();
+                MessageBox.Show("You Saved Earth");
+            }
+
+        }
+        
+        //Procedure: createBullet
+        //input: void
+        //output: void
+        //Description: This procedure produces the bullets that comes out of the cannon
+        private void createBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Image = Properties.Resources.bullet;
+            bullet.Size = new Size(5, 20);
+            bullet.Tag = "bullet";
+            bullet.Left = player.Left + player.Width / 2;
+            bullet.Top = player.Top - 20;
+            this.Controls.Add(bullet);
+            bullet.BringToFront();
+        }
+        //Procedure: gameOver
+        //input: void
+        //output: void
+        //Description:this procedure produces the end of the game
+        private void gameOver()
+        {
+            timer1.Stop();
+            lblScore.Text += "Game Over";
+        }
+
+       
+    }
+
+
+}
